1604655874f3c1fa7ea7fbf899df462c47c74b29
mocthball <56953651+mocthball@users.noreply.github.com>
2023-09-06 09:03:47 +1000

    java for adding grid amended

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 //insert child divs to make grid
-const grid = document.querySelector('grid');
+const grid = document.querySelector('#grid');
 const div = document.createElement('div');
-div.classList.add = "grid_child";
+div.className = "grid_child";
+console.log(div);
 
-for (let x = 0; while x != 16; x++){
+
+for (let x = 0; x != 16; x++){
     grid.appendChild(div);
 }
-
