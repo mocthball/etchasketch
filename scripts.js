ae9701047720a98986296d1170ae04466df70b61
mocthball <56953651+mocthball@users.noreply.github.com>
2023-09-06 19:28:06 +1000

    updated scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 //insert child divs to make grid
+const GRID_SQUARED = 16 * 16;
 const grid = document.querySelector('#grid');
-const div = document.createElement('div');
-div.className = "grid_child";
-console.log(div);
 
 
-for (let x = 0; x != 16; x++){
+for (let x = 0; x != GRID_SQUARED; x++){
+    let div = document.createElement('div');
+    div.className = "grid_child";
+    div.setAttribute("id", x);
+    div.setAttribute("onmousedown", "etch(" + x +")");
     grid.appendChild(div);
+    console.log(div);
+}
+
+function etch(id) {
+    const sketch = document.getElementById(id);
+    sketch.style.cssText = "background: pink;";
 }
